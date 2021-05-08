04c6f03a0185f14697bc1edafe113c38253d3d35
h4ll0w0rld <lepetitfabi@gmail.com>
2021-05-08 11:39:12 +0200

    patch

--- Aufgabe_3_Script/script.js
+++ Aufgabe_3_Script/script.js
@@ -1,7 +1,39 @@
 "use strict";
-// showImage(hans);
-// showImage(mia);
-// showImage(eberle);
+var Aufgabe_1;
+(function (Aufgabe_1) {
+    drawXREcts(3);
+    function randomRecti() {
+        let randomRect = document.createElement("div");
+        randomRect.style.width = Math.random() * 500 + "px";
+        randomRect.style.height = Math.random() * 500 + "px";
+        randomRect.style.backgroundColor = getRandomColor();
+        document.body.append(randomRect);
+    }
+    function getRandomColor() {
+        let inhalt = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+            color += inhalt[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+    function drawXREcts(anzahl) {
+        for (let e = 0; e <= anzahl; e++) {
+            randomRecti();
+        }
+    }
+    //Buttons:
+    let landingButton1 = document.getElementById("landingButton1"); //was ist das für ein Typ ?? 
+    let landingButton2 = document.getElementById("landingButton2");
+    landingButton1.addEventListener("click", drawNewRect);
+    landingButton2.addEventListener("click", clearAllRects);
+    function drawNewRect() {
+        randomRecti();
+    }
+    function clearAllRects() {
+        location.reload();
+    }
+})(Aufgabe_1 || (Aufgabe_1 = {}));
 function showImage(interfacePerson) {
     for (let index = 0; index < interfacePerson.length; index++) {
         let image = document.createElement("img");
@@ -18,27 +50,27 @@ function showImage(interfacePerson) {
         }
         document.body.append(image);
     }
-}
-let heatArray = new Array;
-heatArray[0] = { name: "Hans", secondName: "Peter", imageSrc: "jaja.jpg", specialPart: "head" };
-heatArray[1] = { name: "Eberle", secondName: "Hans", imageSrc: "head1.jpg", specialPart: "head" };
-heatArray[2] = { name: "Mia", secondName: "Kanns", imageSrc: "head2.jpg", specialPart: "head" };
-showImage(heatArray);
-// if (document.head.title == "HeadSelect")
-let speicher = { heat: "test", body: "test", leg: "test" };
-function choseHeat(klick) {
-    let img = klick.currentTarget;
-    console.log(img.src);
-    speicher.heat = img.src;
-}
-function choseBody(klick) {
-    let img = klick.currentTarget;
-    speicher.body = img.src;
-    console.log(img.src);
-}
-function choseLegs(klick) {
-    let img = klick.currentTarget;
-    speicher.leg = img.src;
-    console.log(img.src);
+    let heatArray = new Array;
+    heatArray[0] = { name: "Hans", secondName: "Peter", imageSrc: "jaja.jpg", specialPart: "head" };
+    heatArray[1] = { name: "Eberle", secondName: "Hans", imageSrc: "head1.jpg", specialPart: "head" };
+    heatArray[2] = { name: "Mia", secondName: "Kanns", imageSrc: "head2.jpg", specialPart: "head" };
+    showImage(heatArray);
+    // if (document.head.title == "HeadSelect")
+    let speicher = { heat: "test", body: "test", leg: "test" };
+    function choseHeat(klick) {
+        let img = klick.currentTarget;
+        console.log(img.src);
+        speicher.heat = img.src;
+    }
+    function choseBody(klick) {
+        let img = klick.currentTarget;
+        speicher.body = img.src;
+        console.log(img.src);
+    }
+    function choseLegs(klick) {
+        let img = klick.currentTarget;
+        speicher.leg = img.src;
+        console.log(img.src);
+    }
 }
 //# sourceMappingURL=script.js.map
