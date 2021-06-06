431475f5b7c81c09b71d919deef8b69fc562d682
h4ll0w0rld <lepetitfabi@gmail.com>
2021-06-06 13:06:53 +0200

    Hopfully it works now

--- Aufgabe_2_Server/send.js
+++ Aufgabe_2_Server/send.js
@@ -1,17 +1,25 @@
 "use strict";
-let buttonSubmitt = document.getElementById("submit");
-buttonSubmitt.addEventListener("click", sendTheData);
+let buttonJson = document.getElementById("JsonButton");
+buttonJson.addEventListener("click", showJson);
+let buttonHtml = document.getElementById("HtmlButton");
+buttonHtml.addEventListener("click", showHtml);
+let url;
 async function sendTheData() {
-    console.log("its all fine");
+    console.log("Jup");
     let userData = new FormData(document.forms[0]);
-    let url = "https://myfirsttestserverisnowlive.herokuapp.com/";
-    //  let url: string = "http://localhost:8122";
     let query = new URLSearchParams(userData);
     url += "?" + query.toString();
     let response = await fetch(url, { method: "get" });
     let responseText = await response.text();
-    console.log(responseText);
-    showResponsee(responseText);
+    return responseText;
+}
+async function showJson() {
+    url = "https://myfirsttestserverisnowlive.herokuapp.com/json";
+    console.log(await sendTheData());
+}
+async function showHtml() {
+    url = "https://myfirsttestserverisnowlive.herokuapp.com/html";
+    showResponsee(await sendTheData());
 }
 function showResponsee(response) {
     let responseDiv = document.getElementById("response");
--- Aufgabe_2_Server/send.ts
+++ Aufgabe_2_Server/send.ts
@@ -1,19 +1,38 @@
-let buttonSubmitt: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submit");
-buttonSubmitt.addEventListener("click", sendTheData);
 
-async function sendTheData(): Promise<void> {
-    console.log("its all fine");
+let buttonJson: HTMLButtonElement = <HTMLButtonElement>document.getElementById("JsonButton");
+buttonJson.addEventListener("click", showJson);
+
+let buttonHtml: HTMLButtonElement = <HTMLButtonElement>document.getElementById("HtmlButton");
+buttonHtml.addEventListener("click", showHtml);
+
+let url: string ;
+
+async function sendTheData(): Promise<string> {
+    console.log("Jup");
     let userData: FormData = new FormData(document.forms[0]);
-    let url: string = "https://myfirsttestserverisnowlive.herokuapp.com/";
-    //  let url: string = "http://localhost:8122";
     let query: URLSearchParams = new URLSearchParams(<any>userData);
     url += "?" + query.toString();
     let response: Response = await fetch(url, { method: "get" });
     let responseText: string = await response.text();
-    console.log(responseText);
-    showResponsee(responseText);
+
+    return responseText;
 
 }
+
+async function showJson(): Promise<void> {
+    url = "https://myfirsttestserverisnowlive.herokuapp.com/json";
+    console.log(await sendTheData());
+
+}
+
+
+async function showHtml(): Promise<void> {
+    url = "https://myfirsttestserverisnowlive.herokuapp.com/html";
+    showResponsee(await sendTheData());
+
+}
+
+
 function showResponsee(response: string): void {
     let responseDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("response");
     responseDiv.innerHTML = "Serverantwort: " + response;
